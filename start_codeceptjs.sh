64b11e00b829678d2afb4e7800c7d70bfc57eddb
Alexey Shurov <a73xsh@gmail.com>
2022-04-29 22:00:44 +0400

    adduser

--- start_codeceptjs.sh
+++ start_codeceptjs.sh
@@ -4,3 +4,9 @@ set -ev
 
 echo "test .... ${CODEBUILD_WEBHOOK_HEAD_REF}"
 echo "branch.... ${BUILD_BRANCH}"
+
+groupadd --system nightmare 
+useradd --system --create-home --gid nightmare nightmare
+groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser 
+mkdir -p /home/pptruser/Downloads 
+chown -R pptruser:pptruser /home/pptruser
